a3bf8dcc66b5fe38a590f00dc1036eb6fb1fd9aa
Luiz Lima <luizoscar.lima@gmail.com>
2017-09-07 10:57:01 -0300

    Create web api project

---
+++ P3ImageManagement/P3ImageManagement.Api.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace P3ImageManagement.Api.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
